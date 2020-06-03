adb806ac545d9dc0589eb8a075710ca445c60cec
gopinath-karix <gkraj@karix.com>
2020-06-03 17:40:48 +0530

    - gateway list file download - Timezone api call

--- src/app/gateway-management/gt-listing/gt-listing.component.ts
+++ src/app/gateway-management/gt-listing/gt-listing.component.ts
@@ -3,6 +3,7 @@ import { GatewayManagementService } from '../services/gateway-management.service
 import { GtListing_ApiResponse, GtStatusupdate_ApiResponse, GtListing_TableDataList, GtListing_Data } from '../models/gateway-management.model';
 import { environment } from '../../../environments/environment';
 import Swal from 'sweetalert2';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-gt-listing',
@@ -14,14 +15,17 @@ export class GtListingComponent implements OnInit {
   gatewayDataRes: GtListing_ApiResponse;
   gatewayData: GtListing_Data;
   selectedType: GtListing_TableDataList[];
-  tabledata: GtListing_Data;
 
   constructor(
-    private gatewayManagementService: GatewayManagementService
+    private gatewayManagementService: GatewayManagementService,
   ) { }
 
   ngOnInit() {
     this.GtListing_list();
+  }
+
+  downloadGatewayFile() {
+    this.GtListing_download();
   }
 
   GtListing_list() {
@@ -47,12 +51,28 @@ export class GtListingComponent implements OnInit {
     );
   }
 
+  GtListing_download() {
+    this.gatewayManagementService.GtListing_download().subscribe(
+      (res: any) => {
+        let blob = new Blob([res], { type: 'text/csv' });
+        let fileName = 'GatewayListdata-' + new Date().toLocaleString()
+        saveAs(blob, fileName + ".csv");
+      }, error => {
+        Swal.fire({
+          icon: 'error',
+          title: error.statusText,
+          text: error.message,
+        })
+      }
+    );
+  }
+
   selectType(type) {
     if (type === 'All') {
       this.gatewayData = JSON.parse(JSON.stringify(this.gatewayDataRes));
     } else {
       this.selectedType = this.gatewayDataRes.data.tabledata.filter(item => item.gw_type === type);
-      this.gatewayDataRes.data.tabledata = this.selectedType;
+      this.gatewayData.data.tabledata = this.selectedType;
     }
   }
 
