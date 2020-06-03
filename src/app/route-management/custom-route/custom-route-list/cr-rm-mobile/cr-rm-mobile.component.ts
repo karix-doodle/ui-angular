e484f3863cfa390fb31d614991987e2d4860015d
wgedberg <wgedberg@karix.com>
2020-06-03 19:16:14 +0530

    custom-moblie

--- src/app/route-management/custom-route/custom-route-list/cr-rm-mobile/cr-rm-mobile.component.ts
+++ src/app/route-management/custom-route/custom-route-list/cr-rm-mobile/cr-rm-mobile.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomService } from 'src/app/route-management/services/custom.service';
-import { MobileCustom, MobileCustomList, MobileListData } from 'src/app/route-management/models/custom.model';
+import { CustomService } from 'src/app/route-management/services/RouteManagement/custom-route/custom.service';
+import {  MobileList_Data, MobileCustom_List, MobileCustom_ApiResponse } from 'src/app/route-management/models/custom.model';
 import Swal from 'sweetalert2';
+import { MobileCustomRouteService } from 'src/app/route-management/services/RouteManagement/custom-route/mobile-custom-route.service';
+import { ToastrManager } from 'ng6-toastr-notifications';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cr-rm-mobile',
@@ -10,12 +13,12 @@ import Swal from 'sweetalert2';
 })
 export class CrRmMobileComponent implements OnInit {
 
-  constructor(public customService: CustomService) { }
-
+  constructor( public mobileCustomRoute: MobileCustomRouteService, public toastr: ToastrManager ) { }
+  searchvalue: '';
   sortingName: string;
   isDesc: boolean;
-
-  mobileCustomData: MobileCustomList;
+  mobileCustomApiResponse: MobileCustom_ApiResponse;
+  mobileCustomData: MobileCustom_List;
 
   ngOnInit() {
     this.getMobileCustomRoute();
@@ -25,9 +28,33 @@ export class CrRmMobileComponent implements OnInit {
    * @description gets all the mobile custom route data
    */
   getMobileCustomRoute() {
-    this.customService.getMobileCustomRoute().subscribe((res: MobileCustom) => {
-      this.mobileCustomData = res.data;
+    this.mobileCustomRoute.getCustomMobileList().subscribe((res: MobileCustom_ApiResponse) => {
+      console.log(res);
+      if (
+        res.responsestatus === environment.APIStatus.success.text &&
+        res.responsecode > environment.APIStatus.success.code
+      ) {
+        this.mobileCustomApiResponse = res;
+        this.mobileCustomData = res.data;
+      } else if (
+        res.responsestatus === environment.APIStatus.error.text &&
+        res.responsecode < environment.APIStatus.error.code
+      ) {
+        Swal.fire({
+          icon: 'error',
+          title: res.responsestatus,
+          text: res.responsestatus,
+        });
+      }
+    },
+    (error) => {
+      Swal.fire({
+        icon: 'error',
+        title: error.statusText,
+        text: error.message,
+      });
     });
+
   }
 
   /**
@@ -35,10 +62,10 @@ export class CrRmMobileComponent implements OnInit {
    * @param mobilerouteData consists of mobile custom data to delete
    * @description deletes the selected mobile custom route data
    */
-  deleteMobileCustom(mobilerouteData: MobileListData) {
+  deleteMobileCustom(mobilerouteData: MobileList_Data) {
     Swal.fire({
       title: 'Are you sure?',
-      text: "You won't be able to revert this!",
+      text: 'You won\'t be able to revert this!',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
@@ -46,19 +73,28 @@ export class CrRmMobileComponent implements OnInit {
       confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
       if (result.value) {
-
+        const mobileData = {
+          id: mobilerouteData.id, mobile: mobilerouteData.mobile, username: '1234'
+        };
+        this.mobileCustomRoute.deleteCustomMobile(mobileData).subscribe((data: any) => {
+          if (data.responsestatus === 'failure') {
+            this.toastr.errorToastr(data.message, '', { position: 'bottom-right' });
+          } else {
+            this.toastr.successToastr(data.message, '', { position: 'bottom-right' });
+            this.getMobileCustomRoute();
+          }
+        });
       }
-    })
+    });
   }
 
   /**
-   * 
+   *
    * @param tableHeaderName consists of table header
    * @description sorts the table based upon the table Header Name
    */
   sort(tableHeaderName: string): void {
-    if (tableHeaderName && this.sortingName !== tableHeaderName) this.isDesc = false;
-    else this.isDesc = !this.isDesc;
+    if (tableHeaderName && this.sortingName !== tableHeaderName) { this.isDesc = false; } else { this.isDesc = !this.isDesc; }
     this.sortingName = tableHeaderName;
   }
 }
---
+++ src/app/route-management/services/RouteManagement/custom-route/custom.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {
+  CustomSummary_ApiResponse,
+  CustomGateway_ApiResponse,
+} from '../../../models/custom.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomService {
+  baseUrl: string = environment.serverUrl + '/routemgmt/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }),
+  };
+  user = {
+    loggedinusername: environment.loggedinusername,
+    loggedinempid: environment.loggedinempid,
+  };
+
+  constructor(public http: HttpClient) {}
+
+  /**
+   * @description gets the custom route summary
+   */
+  getCustomRouteSummary(): Observable<CustomSummary_ApiResponse> {
+    return this.http
+      .post(this.baseUrl + '/custom/summary', this.user, this.httpOptions)
+      .pipe(map((data) => data as CustomSummary_ApiResponse));
+  }
+
+  getCustomRouteGateways(): Observable<CustomGateway_ApiResponse> {
+    return this.http
+      .post(this.baseUrl + '/listgateways', { ...this.user,  ...{ gw_type: 'direct' } }, this.httpOptions)
+      .pipe(map((data) => data as CustomGateway_ApiResponse));
+  }
+}
