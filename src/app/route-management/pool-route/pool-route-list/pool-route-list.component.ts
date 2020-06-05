3d6013e260e88057c1836bbadc96319c008f35e4
MohammedRahimA <amrahim@karix.com>
2020-06-05 20:21:51 +0530

    Delete a pool route functionality added with alert.

--- src/app/route-management/pool-route/pool-route-list/pool-route-list.component.ts
+++ src/app/route-management/pool-route/pool-route-list/pool-route-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PoolRouteService } from '../../services/RouteManagement/poolRoute/pool-route.service';
-import { PoolRouteListRes, TaggedAccountsList } from '../../models/RouteManagement/PoolRoute/poolRoute';
+import { PoolRouteListRes, TaggedAccountsList, PoolRouteRes, RoutesRowlist } from '../../models/RouteManagement/PoolRoute/poolRoute';
 import { environment } from '../../../../environments/environment';
 import Swal from 'sweetalert2';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -46,6 +46,8 @@ export class PoolRouteListComponent implements OnInit {
 
   ngOnInit() {
     this.loadList();
+    // this.isDesc = true;
+    // this.isDeselect = true;
   }
   /**
    * @description gets the pool route list
@@ -74,6 +76,51 @@ export class PoolRouteListComponent implements OnInit {
         });
       }
     );
+  }
+
+  onDelete(routeId, routeName) {
+    // console.log(index);
+    // console.log(list[index]);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: `You won't be able to revert ${routeName}!`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.poolRouteService.deletePoolRoute({ loggedinempid: environment.loggedinempid, route_id: routeId })
+          .subscribe((res: PoolRouteRes) => {
+            if (res.responsestatus === environment.APIStatus.success.text
+              && res.responsecode > environment.APIStatus.success.code) {
+              Swal.fire({
+                icon: 'success',
+                title: 'Deleted!',
+                text: res.message
+              });
+              // this.poolListRes.data.routes_list.splice(index, 1);
+              this.poolListRes.data.routes_list = this.poolListRes.data.routes_list
+                .filter((element) => element.route_id !== routeId);
+            } else if (
+              res.responsestatus === environment.APIStatus.error.text
+              && res.responsecode < environment.APIStatus.error.code
+            ) {
+              Swal.fire({
+                icon: 'error',
+                title: res.responsestatus,
+                text: res.message,
+              });
+            }
+          }, (error: HttpErrorResponse) => {
+            Swal.fire({
+              icon: 'error',
+              text: error.message,
+            });
+          });
+      }
+    });
   }
 
   /**
--- src/app/route-management/services/RouteManagement/poolRoute/pool-route.service.ts
+++ src/app/route-management/services/RouteManagement/poolRoute/pool-route.service.ts
@@ -4,7 +4,7 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from '../../../../shared/models/commonModels';
-import { PoolRouteListRes, SelectedPoolRoute, SelectedPoolRouteRes } from '../../../models/RouteManagement/PoolRoute/poolRoute';
+import { PoolRouteListRes, SelectedPoolRoute, SelectedPoolRouteRes, PoolRouteRes } from '../../../models/RouteManagement/PoolRoute/poolRoute';
 
 @Injectable({
   providedIn: 'root'
@@ -28,10 +28,19 @@ export class PoolRouteService {
       .pipe(map(m => m as PoolRouteListRes));
   }
   /**
+   * @param input consists of route id and login emp id
    * @description gets the list of particular pool route details
    */
   viewPoolRouteDetailsList(input: SelectedPoolRoute): Observable<SelectedPoolRouteRes> {
     return this.http.post(this.baseUrl + 'viewpoolroute', input, this.httpOptions)
       .pipe(map(m => m as SelectedPoolRouteRes));
+  }
+  /**
+   * @param input consists of route id and login emp id
+   * @description to delete particular pool route
+   */
+  deletePoolRoute(input: SelectedPoolRoute): Observable<PoolRouteRes> {
+    return this.http.post(this.baseUrl + 'deletepoolroute', input, this.httpOptions)
+      .pipe(map(m => m as PoolRouteRes));
   }
 }
