312f87b56e7bea67df3ba8c21ba064eb1f9461da
Hema Ramachandran <rhemalatha@karix.com>
2020-06-15 15:33:17 +0530

    changes

--- src/app/customer-management/cm-list/cm-list.module.ts
+++ src/app/customer-management/cm-list/cm-list.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
 import { CmListRoutingModule } from './cm-list-routing.module';
 import { CmListTableComponent } from './cm-list-table/cm-list-table.component';
@@ -10,6 +11,7 @@ import { PendingActivateTableComponent } from './pending-activate-table/pending-
   declarations: [ CmListTableComponent, PendingActivateTableComponent],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
     CmListRoutingModule
   ]
