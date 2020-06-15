26fd0880c4c327d1bbe19d8ffa4d258c21ad0666
Tayyab Shaik <tshaik@karix.com>
2020-06-15 15:06:39 +0530

    cust mgmt - pending for activation page ui integration changes

--- src/app/customer-management/cm-list/cm-list.module.ts
+++ src/app/customer-management/cm-list/cm-list.module.ts
@@ -4,14 +4,15 @@ import { SharedModule } from '../../shared/shared.module';
 import { CmListRoutingModule } from './cm-list-routing.module';
 import { CmListTableComponent } from './cm-list-table/cm-list-table.component';
 import { PendingActivateTableComponent } from './pending-activate-table/pending-activate-table.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ CmListTableComponent, PendingActivateTableComponent],
   imports: [
     CommonModule,
     SharedModule,
-    CmListRoutingModule
+    CmListRoutingModule,
+    FormsModule
   ]
 })
 export class CmListModule { }
