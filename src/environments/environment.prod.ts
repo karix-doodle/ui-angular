678b212305c6ec40cc651e5e07009368f7421c73
wgedberg <wgedberg@karix.com>
2020-07-22 21:26:30 +0530

    blacklist file upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,41 @@
 export const environment = {
-  production: true
+  production: true,
+  serverUrl: 'http://115.114.108.242/api/intlmgmt',
+  //serverUrl: 'http://10.20.59.222:8383/api/intlmgmt',
+  FileUploadUrl: 'http://10.20.51.182:8484/api/intlmgmt',
+  loggedinusername: 'testuser',
+  loggedinempid: 1234,
+  UTC: "+05:30",
+  APIStatus: {
+    success: {
+      text: 'success',
+      code: 0
+    },
+    error: {
+      text: 'failure',
+      code: 0
+    }
+  },
+  currencyDefault: 14,
+  senderIdType: {
+    0: 'Open',
+    1: 'List',
+    2: 'Static',
+    3: 'Numeric',
+  },
+  createClonePoolRouteFieldLength: {
+    routeNameInputBox: {
+      min: 3,
+      max: 15
+    },
+    commentsTextArea: {
+      min: 3,
+      max: 1000
+    }
+  },
+  charsetEncode: ['GSM', 'ISO-8859-1', 'ASCII'],
+  defaultCharsetEncode: 'GSM',
+  dlrType: ['No DLR', 'Handset DLR', 'SMSC DLR'],
+  customerTypes: ['Direct', 'Premium', 'Wholesale'],
+  dateTimePickerTimeDifference: 10
 };
