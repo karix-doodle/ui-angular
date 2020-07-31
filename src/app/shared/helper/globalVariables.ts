d4986458e3b252d35a23a1d45d88f50d5f0ed556
MohammedRahimA <amrahim@karix.com>
2020-07-31 15:57:06 +0530

    PoolRoute configurable static helper variables added.

--- src/app/shared/helper/globalVariables.ts
+++ src/app/shared/helper/globalVariables.ts
@@ -2,7 +2,29 @@
  * Gateway management START
  */
 
- // Write the global validables
+// Write the global variables
 
 /* Gateway management END */
+
+
+
+/* Route management Pool route */
+export const poolRouteHelper = {
+    createClonePoolRouteFieldLength: {
+        routeNameInputBox: {
+            min: 3,
+            max: 15
+        },
+        commentsTextArea: {
+            min: 3,
+            max: 1000
+        }
+    },
+    createClonePoolRouteGatewaysList: [
+        { name: 'Direct', val: 'direct' },
+        { name: 'Premium', val: 'premium' },
+        { name: 'Wholesale', val: 'wholesale' }
+    ]
+};
+/* Route management Pool route */
 
